33498f68ddb914627ba18e15b00979d4945eb21d
ashish <ashishrajput142@gmail.com>
2025-06-01 13:32:01 +0530

    Az cmds

---
+++ createVm.ps1
@@ -0,0 +1,10 @@
+﻿$rgname = Read-Host "Enter resource group name"
+$name = Read-Host "Enter Vm name"
+$image = Read-Host "image name"
+$location = Read-Host "enter location name"
+$ceredential = Read-Host "Enter ceredential name"
+$port = Read-Host "enter port number"
+$publicipaddress =  Read-Host "Enter public ip address"
+
+
+New-AzVM -Credential $ceredential -Image $image -Location $loction -Name $name -OpenPorts $port -PublicIpAddressName $publicipaddress -ResourceGroupName $rgname
---
+++ rg_loc.ps1
@@ -0,0 +1,7 @@
+﻿$rgname = Read-Host "Enter the resource group name"
+$location = Read-Host "Enter the Location Name"
+
+New-AzResourceGroup -Name $rgname -Location $location
+
+#print on console
+Write-Host $rgname "has been created sucessfully"
